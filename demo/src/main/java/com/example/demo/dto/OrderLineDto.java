5888f1c29e23550885ec43c5c18cc7d5b8471634
AGILDA <gilda.kulli@gmail.com>
2023-11-17 11:07:13 +0100

    Merge remote changes

--- demo/src/main/java/com/example/demo/dto/OrderLineDto.java
+++ demo/src/main/java/com/example/demo/dto/OrderLineDto.java
@@ -3,7 +3,11 @@ package com.example.demo.dto;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class OrderLineDto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- demo/src/main/java/com/example/demo/dto/UserDto.java
+++ demo/src/main/java/com/example/demo/dto/UserDto.java
@@ -6,7 +6,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UserDto {
     @jakarta.persistence.Id
     @GeneratedValue
